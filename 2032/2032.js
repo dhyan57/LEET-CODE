9fc2fff5a3341f68a40a4ce7d84b8a810f6e5ca9
dhyan57 <dhyanpramod2@gmail.com>
2025-05-23 00:52:54 +0530

    completed 2824

--- 2032/2032.js
+++ 2032/2032.js
@@ -1,8 +1,8 @@
-@param {number[]} nums1
- * @param {number[]} nums2
- * @param {number[]} nums3
- * @return {number[]}
- */
+// @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @param {number[]} nums3
+//  * @return {number[]}
+//  */
 var twoOutOfThree = function(nums1, nums2, nums3) {
     let arr=[]
     for(i=0;i<nums1.length;i++){
